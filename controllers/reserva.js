dd162307737c99f1bf27c93ba412e0916cf8b493
DavidVallecios <00078217@uca.edu.sv>
2019-11-12 22:25:05 -0600

    [Controllers] creacion de los controladores

---
+++ controllers/reserva.js
@@ -0,0 +1,10 @@
+const Reserva = require('../models/reserva')
+
+
+module.exports = {
+    getAll,
+    getOneById,
+    insert,
+    update,
+    erase
+}
--- js/main.js
+++ js/main.js
@@ -149,3 +149,6 @@ window.onload = () => {
       
         calendar.render();
        });
+
+
+       
