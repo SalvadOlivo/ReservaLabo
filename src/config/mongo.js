884ea5460a298928971d97a3a5d015584b8a4f9f
DavidVallecios <00078217@uca.edu.sv>
2019-11-26 11:58:29 -0600

    prueba 2?

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -7,7 +7,7 @@ let uri = `mongodb://${host}/${database}`
 
 mongoose.set('useFindAndModify', false);
 const connect = ()=>{
-    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then( () => {
         console.log('Db connected')
     })
